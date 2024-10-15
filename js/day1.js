18fb58c5cc872dc9009fde8a86110da48fc42df6
abdull-ah47 <kaleemabdullah002@gmail.com>
2024-10-15 15:34:49 +0500

    project updated

---
+++ js/day1.js
@@ -0,0 +1,26 @@
+var name = "Ali";
+console.log(name);  
+
+var name = "saad";   
+console.log(name);  
+
+name = "ahmed";   
+console.log(name); 
+ 
+let age = 25;
+console.log(age);  
+
+age = 30;          
+console.log(age);  
+
+const car = {
+    brand: "Toyota",
+    model: "Corolla"
+  };
+  
+  console.log(car.brand); 
+
+  car.model = "Camry";
+  console.log(car.model);  
+  
+  
