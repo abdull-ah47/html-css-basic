f8b8166e1d7c3437fa0cd59f08e719a50d88d3a5
abdull-ah47 <kaleemabdullah002@gmail.com>
2024-10-17 00:13:31 +0500

    project updated

---
+++ week1/day3.js
@@ -0,0 +1,121 @@
+//simple assignment operatoer
+let x = 5;       
+let name = "John";  
+let flag = true;   
+let z = 3.14;       
+let y = x;          
+
+//addition operator
+let a = 10;
+a += 5;        
+
+let b = "Hello";
+b += " World";      
+
+let c = 20;
+c += 2;            
+
+let d = 3;
+d += d;             
+
+let e = 7;
+e += 10;            
+
+//subtraction opertor
+let f = 20;
+f -= 5;             
+
+let g = 100;
+g -= 30;           
+
+let h = 7;
+h -= 2;             
+
+let i = 50;
+i -= 25;            
+
+let j = 8;
+j -= 1;             
+
+//multiplication operator
+let k = 5;
+k *= 3;             
+
+let l = 2;
+l *= 4; 
+
+let m = 7;
+m *= 2;             
+
+let n = 10;
+n *= 5;            
+
+let o = 3;
+o *= o;            
+//division operator
+
+let p = 100;
+p /= 5;        
+let q = 50;
+q /= 2;            
+
+let r = 36;
+r /= 6;             
+
+let s = 81;
+s /= 9;             
+
+let t = 64;
+t /= 8;           
+
+//comparison operators
+//equall
+console.log(5 == "5");       
+console.log(10 == 10);      
+console.log("abc" == "abc");
+console.log(0 == false);     
+console.log(null == undefined); 
+
+
+//strict equal
+console.log(5 === "5");   
+console.log(10 === 10);   
+console.log(true === 1);     
+console.log("abc" === "abc");
+console.log(null === undefined); 
+
+//not equal
+console.log(5 != "5");      
+console.log(10 != 15);      
+console.log("abc" != "ABC"); 
+console.log(0 != false);     
+console.log(null != undefined); 
+
+//strict not equal
+console.log(5 !== "5");     
+console.log(10 !== 10);    
+console.log(true !== 1);    
+console.log("abc" !== "ABC");
+console.log(null !== undefined); 
+
+//greater than
+console.log(10 > 5);       
+console.log(3 > 8);          
+console.log(7 > 7);          
+console.log("b" > "a");      
+console.log(100 > "50");     
+
+//greater than equal to
+console.log(10 >= 5);      
+console.log(7 >= 7);         
+console.log(3 >= 8);        
+console.log("b" >= "a");     
+console.log(50 >= "50");     
+
+
+//less than equal to
+console.log(5 <= 10);    
+console.log(8 <= 3);         
+console.log(7 <= 7);         
+console.log("a" <= "b");     
+console.log(50 <= "50");     
